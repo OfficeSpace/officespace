94351dc5a3a8eb7ebcd6dffeeea0b9f4f7f76177
Kirk Johnson <kirkjohnson21@yahoo.com>
2018-12-18 11:33:00 -0800

    Styling changes

--- template-parts/header/site-branding.php
+++ template-parts/header/site-branding.php
@@ -9,13 +9,18 @@
  */
 
 ?>
-<div>
+<div class="site-branding">
+	<div class="wrap">
 
 		<?php the_custom_logo(); ?>
 
+		<div class="site-branding-text">
+
+		</div><!-- .site-branding-text -->
 
 		<?php if ( ( twentyseventeen_is_frontpage() || ( is_home() && is_front_page() ) ) && ! has_nav_menu( 'top' ) ) : ?>
 		<a href="#content" class="menu-scroll-down"><?php echo twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ); ?><span class="screen-reader-text"><?php _e( 'Scroll down to content', 'twentyseventeen' ); ?></span></a>
 	<?php endif; ?>
 
+	</div><!-- .wrap -->
 </div><!-- .site-branding -->
