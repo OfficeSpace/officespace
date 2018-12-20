232588cbcab48aa977e8a02de5dc43387f6225c6
Kirk Johnson <kirkjohnson21@yahoo.com>
2018-12-20 13:14:30 -0800

    Styling and footer

--- template-parts/footer/site-info.php
+++ template-parts/footer/site-info.php
@@ -9,6 +9,16 @@
  */
 
 ?>
-<div class="site-info">
-	<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a>
-</div><!-- .site-info -->
+
+<div class="component page-footer">
+  <div class="copyright">
+    All Content © 2018 <a href="/">OfficeSpace.com</a>, All Rights Reserved
+
+  </div>
+  <div class="navigation">
+    <div class="faq"><a href="/pages/faq">FAQ</a></div>
+    <div class="blog"><a href="/blog">Blog</a></div>
+    <div class="about"><a href="/pages/about">About</a></div>
+    <!-- .contact= link_to "Contact", "/pages/contact" -->
+  </div>
+</div>
