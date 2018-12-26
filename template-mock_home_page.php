8f4b382347b4019da48369af9b26f8f5d9ab2881
Kirk Johnson <kirkjohnson21@yahoo.com>
2018-12-26 09:49:57 -0800

    Remove unused code

--- template-mock_home_page.php
+++ template-mock_home_page.php
@@ -45,11 +45,7 @@ get_header();?>
           'current' => $paged,
           'total' => $wpb_all_query->max_num_pages
         ) );
-        // the_posts_pagination( array(
-        //   'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-        //   'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-        //   'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-        // ) );
+
 
       else :
 
